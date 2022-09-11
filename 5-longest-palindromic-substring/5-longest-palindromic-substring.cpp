441ddf90af9dba3e375062f3177c2a3ce2f5eef5
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-09-11 05:52:00 +0530

    Time: 766 ms (15.50%), Space: 283 MB (12.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -9,11 +9,16 @@ public:
         for(int diff = 0;diff<n;diff++) {
             for(int i=0,j=i+diff; j<n; i++, j++)
             {
-                if(i==j) dp[i][j]=1;
-                else if(diff==1) dp[i][j]=(s[i]==s[j])?2:0;
+                if(i==j) {
+                    dp[i][j]=1;
+                }
+                else if(diff==1){
+                    dp[i][j]=(s[i]==s[j])?1:0;   
+                } 
                 else {
                     if(s[i]==s[j] && dp[i+1][j-1])
-                        dp[i][j] = dp[i+1][j-1]+2; }
+                        dp[i][j] = dp[i+1][j-1]; 
+                }
         
                 if(dp[i][j]) { 
                     if(j-i+1>maxlength) {
