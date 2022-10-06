c42b97cf34af10db860583e682bbcfd3394e197f
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-10-07 04:27:25 +0530

    Time: 259 ms (44.59%), Space: 53.3 MB (76.35%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,40 +1,50 @@
 class Solution {
+    bool ispalindrome(int start,int end,string &s){
+        while(start<end){
+            
+            if(s[start]!=s[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+        
+    }
+    
+    void f(int ind,vector<string>&ds,vector<vector<string>>&ans,string s,int n){
+        if(ind==n){
+            ans.push_back(ds);
+            return;
+        }
+        
+        //pation at a call possible from index i=ind to n-1
+        
+        for(int i=ind;i<n;i++){
+            
+            //start index =ind ending =i
+            if(ispalindrome(ind,i,s)){
+                
+                string sub=s.substr(ind,i-ind+1);
+                ds.push_back(sub);
+                f(i+1,ds,ans,s,n);
+                ds.pop_back();
+                
+            }    
+        }
+           
+    }
+    
 public:
-	vector<vector<string>> partition(string s) {
-		vector<vector<string>> ans;
-		vector<string>path;
-		int n = s.size();
-		solve(0, n, s, path, ans);
-		return ans;
-	}
-
-	void solve(int index, int n, string s, vector<string>&path, vector<vector<string>>&ans)
-	{
-		if (index == n)
-		{
-			ans.push_back(path);
-			return;
-		}
-		for (int i = index; i < n; i++)
-		{
-			if (s[i] == s[index] && ispalindrome(index, i, s))
-			{
-				path.push_back(s.substr(index, i - index + 1));
-				solve(i + 1, n, s, path, ans);
-				path.pop_back();
-			}
-		}
-	}
-
-	bool ispalindrome(int start, int end, string &s)
-	{
-		while (start < end)
-		{
-			if (s[start++] != s[end--])
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+    vector<vector<string>> partition(string s) {
+        
+        vector<vector<string>>ans;
+        vector<string> ds;
+        int n=s.length();
+        f(0,ds,ans,s,n);
+        return ans;
+        
+         
+        
+    }
 };
