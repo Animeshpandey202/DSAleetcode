32ec941fddfa1ef4cef50b10d05cfd76c2baf45b
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-04 00:14:27 +0530

    Time: 205 ms (83.03%), Space: 71.3 MB (24.68%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,30 +1,58 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) 
+    int totalFruit(vector<int>& s) 
     {
-        int i = 0, j = 0, k = 2, sz = fruits.size(), maxm = 0 ;
-        unordered_map<int, int> umap ;
+        int i = 0, j = 0, k = 2, sz = s.size(), maxi = 0 ;
+        unordered_map<int,int>mp;
         
-        while(j<sz)
-        {
-            umap[fruits[j]]++ ;
+        for(int j=0;j<sz;j++){
             
-            if(umap.size() <= k)
-            {
-                maxm = max(maxm, j-i+1) ; j++ ;
-            }
-            else if(umap.size() > k)
-            {
-                while(umap.size() > k)
-                {
-                    umap[fruits[i]]-- ;
-                    if(umap[fruits[i]] == 0){ umap.erase(fruits[i]) ; }
-                    i++ ;
+            //acquire
+            mp[s[j]]++;
+            
+            
+            //remove if invalid 
+            
+            if(mp.size()>k){
+                
+                while(mp.size()>k){
+                    
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0)mp.erase(s[i]);
+                    i++;
                 }
-                if(umap.size()==k)maxm = max(maxm, j-i+1) ;
-                j++ ;
+                
             }
+            maxi=max(maxi,j-i+1);
+            
+            
         }
-        return maxm ;
+        
+        return maxi;
+         
+      
     }
 };
+//   unordered_map<int, int> umap ;
+        
+//         while(j<sz)
+//         {
+//             umap[fruits[j]]++ ;
+            
+//             if(umap.size() <= k)
+//             {
+//                 maxm = max(maxm, j-i+1) ; j++ ;
+//             }
+//             else if(umap.size() > k)
+//             {
+//                 while(umap.size() > k)
+//                 {
+//                     umap[fruits[i]]-- ;
+//                     if(umap[fruits[i]] == 0){ umap.erase(fruits[i]) ; }
+//                     i++ ;
+//                 }
+//                 if(umap.size()==k)maxm = max(maxm, j-i+1) ;
+//                 j++ ;
+//             }
+//         }
+//         return maxm ;
