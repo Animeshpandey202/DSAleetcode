e363577ef8558a92c70a98f4000dabfd18750b37
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-05 05:11:09 +0530

    Time: 187 ms (18.92%), Space: 42.3 MB (96.90%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,26 +2,54 @@ class Solution {
 public:
     int maxScore(vector<int>& s, int k) {
         int mini=INT_MAX;
-        int n=s.size();
-        int prefix[n]; prefix[0]=s[0];
-        
-        for(int i=1;i<n;i++){
-            prefix[i]=s[i]+prefix[i-1];
+        int m=s.size()-k;
+        int sum=0;
+        int total=0;
+        for(int i=0;i<s.size();i++){
+            total+=s[i];
         }
-        if(n==k)return prefix[n-1];
-        int i=0;
-        for(int j=n-k-1;j<n;j++){
+        for(int i=0;i<m;i++){
+            sum+=s[i];
+        }
+        
+        mini=min(mini,sum);
+        
+        for(int i=m;i<s.size();i++){
+            //remove overflowing el
+            sum-=s[i-m];
             
-            int sum=prefix[j]-prefix[i]+s[i];
-            
+            sum+=s[i];
             
             mini=min(mini,sum);
-           
             
-            i++;
         }
         
-        return prefix[n-1]-mini;
+        return total-mini;
         
+
     }
 };
+
+
+
+// // int mini=INT_MAX;
+//         int n=s.size();
+//         int prefix[n]; prefix[0]=s[0];
+        
+//         for(int i=1;i<n;i++){
+//             prefix[i]=s[i]+prefix[i-1];
+//         }
+//         if(n==k)return prefix[n-1];
+//         int i=0;
+//         for(int j=n-k-1;j<n;j++){
+            
+//             int sum=prefix[j]-prefix[i]+s[i];
+            
+            
+//             mini=min(mini,sum);
+           
+            
+//             i++;
+//         }
+        
+//         return prefix[n-1]-mini;
