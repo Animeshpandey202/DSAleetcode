c30fa263f0b0cdec9246c9ea492b5070a9c87793
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-04 20:41:06 +0530

    Time: 361 ms (49.31%), Space: 67.7 MB (75.11%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-
-        int n = nums.size();
-
-        // total number of calid subarray
-        int res = 0;
-        
-        // count the odd numbers in the window
-        int cnt = 0;
-        
-        // first and last pointer of sliding window
-        int i = 0;
-        int j = 0;
-        while(j<n)
-        {
-            // update count of the odd number in window
-            if((nums[j++]&1)==1)cnt++;            
-            
-            // shrink window until odd number count == k
-            while(cnt>k)if((nums[i++]&1)==1)cnt--;      
-            
-            // if odd number count == k then we found our main subarray
-            if(cnt==k)
-            {
-                int lc = 0, rc = 0;
-                
-                // shrink window from left till any odd number found
-                // means, count number of even number at the left of first odd number in window, EL
-                while(j<n && (nums[j]&1)==0)j++,rc++;
-
-                // expand window to right till any odd number found
-                // means, count number of even number at the right of last odd number in window, ER
-                while(i<n && (nums[i]&1)==0)i++,lc++;
-                
-                // based on probability formula the total possible subarray inclusing all odd numbers
-                // of the main subarray window is (EL + 1) * (ER + 1)
-                res+= ((lc+1) * (rc+1));
+    int numarr(vector<int>&nums,int k){
+        int ans=0;
+        int count=0;
+        int i=0,j=0,n=nums.size();
+        while(j<n){
+            if(nums[j]%2==1){
+                count++;
             }
+            if(count>k){
+                while(i<=j && count>k){
+                    if(nums[i]%2==1){
+                        count--;
+                    }
+                    i++;
+                }
+            }
+            ans+=(j-i+1);
+            j++;
         }
-        
-        return res;
+        return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return numarr(nums,k)-numarr(nums,k-1);
+
     }
 };
