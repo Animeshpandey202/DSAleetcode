0b6135ea624f68398b094d445bb4df863b3113b2
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-06 03:44:28 +0530

    Time: 68 ms (20.50%), Space: 8.6 MB (20.58%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -11,50 +11,58 @@ string minWindow(string s, string t) {
     }
 
     //These 2 unorders maps will store frequency of characters in t string and windows
-    unordered_map<char, int> map;
-    unordered_map<char, int> window;
+    unordered_map<char, int> mpt;
+    unordered_map<char, int> mps;
 
     //It will update the frequency of each charater of string t int map;
     for(int i = 0; i < t.length(); i++){
-        map[t[i]]++;
+        mpt[t[i]]++;
     }
+    cout<<mpt['D'];
 
     //variable to store minimum length of result string
-    int minLength = INT_MAX;
+    int mini = INT_MAX;
 
     //counter
     int letterCounter = 0;
-    
-    for(int slow = 0, fast = 0; fast < s.length(); fast++){
+    int i=0;
+    for(int j= 0;j < s.length(); j++){
         
         //This c variable will store each character of string s according to the fast pointer movement starting from 0
-        char c = s[fast];
+        char c = s[j];
         //store that character in current window and increase the counter by 1
         
-        window[c]++;
-        //This if condition will execute only if there is need to fulfill required charters from map to window and increase             the letter counter
-            if(window[c] <= map[c]){
+        mps[c]++;
+        //This if condition will execute only if there was need to fulfill required charters from map to window and increase             the letter counter
+            if(mpt.find(c)!=mpt.end() && mps[c] <= mpt[c]){
                 letterCounter++;
             }
       
 
         //This condition we have found the result in current window
         if(letterCounter == t.length()){
-           
-            while (map.find(s[slow]) == map.end() || window[s[slow]] > map[s[slow]])
+              //It will update the minimum required substring
+            if(j-i + 1 < mini){
+                mini = j - i + 1;
+                //It will update the result string
+                result = s.substr(i, mini);
+            }
+            
+           //if extra character in window(el not in t or present in t but count in  s window more than t )
+            while (mpt.find(s[i])==mpt.end() || mps[s[i]] > mpt[s[i]])
             {
                 //This will start decreasing frequency of extra character in window
-                window[s[slow]]--;
+                mps[s[i]]--;
 
                 //and move slow pointer
-                slow++;
+                i++;
             }
 
             //It will update the minimum required substring
-            if(fast-slow + 1 < minLength){
-                minLength = fast - slow + 1;
+            if(j-i + 1 < mini){
+                mini = j - i + 1;
                 //It will update the result string
-                result = s.substr(slow, minLength);
+                result = s.substr(i, mini);
             }
         }
     }
