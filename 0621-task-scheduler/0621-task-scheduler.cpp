60c8adf52c4acfc339528d564ab2f2a6a9e002a8
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-25 04:53:25 +0530

    Time: 137 ms (72.09%), Space: 43.8 MB (18.35%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,38 +2,51 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if(n==0) return tasks.size();
-        unordered_map<char,int>m;
-        for(auto ch : tasks)
-        {
-            if(m.find(ch)==m.end())
-                m[ch]=0;
-            
-            m[ch]++;//adding the count of every character to the map
+        //TC=O(nlogn)  SC=O(N)
+        if(n==0)return tasks.size();
+        
+        unordered_map<char,int>mp;
+        for(auto it:tasks){
+            //adding the count of every character to the map
+            mp[it]++;
         }
-        priority_queue<int> q;
-        for(auto x:m)
-            q.push(x.second);
-        int cycles=0;
-        while(q.empty()==false)
-        {
-            vector<int>temp;
-            for(int i=0;i<=n;i++)//running our process for the cooldown process
-            {
-                if(q.empty()==false)
-                {
-                    temp.push_back(q.top());//we are seeing the tasks that we are processing during this cooldown pweriod
-                    q.pop();
+        priority_queue<int>pq;
+        for(auto it:mp){
+            pq.push(it.second);
+        }
+        int ans=0;
+        
+        while(!pq.empty()){
+             //v will store type of task done in a cooldown time
+            vector<int>v;
+            
+            //add tasks
+            //if cooldown time is 2...then 3 type of task can be done in this time....if 4 then 5 type task
+            
+            for(int i=0;i<=n;i++){
+                if(!pq.empty()){
+                    v.push_back(pq.top());
+                    pq.pop();
                 }
             }
-            for(int i:temp)
-            {
-                if(--i>0)
-                    q.push(i);
+            
+            
+            
+            //if tasks left to be done add back to pq
+            for(auto it:v){
+                //if after running a type of task for 1 time its still left...again push in pq
+                if(--it>0){
+                    pq.push(it);
+                }
             }
-            cycles+= (q.empty()==true)? temp.size():n+1;
+            
+            //add to ans
+            //exp if tasks= A B  and n=2 then both task processed in 1st cooldown period with 1unit time remaining ....therefore ans+= no.of task ... dry run 1st exp
+            
+            ans+= (pq.empty()==true)?v.size():n+1;
         }
-        
-        return cycles;
+        return ans;
     }
 };
+
+  
