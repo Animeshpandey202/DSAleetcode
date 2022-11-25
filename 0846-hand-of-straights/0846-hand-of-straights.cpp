28607879c8dbd806f530e3a67c28bb5c70dfc235
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-25 17:27:18 +0530

    Time: 198 ms (14.51%), Space: 28.1 MB (81.01%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        // create a min heap
-		priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        // create unordered map to store integer counts
-		unordered_map<int,int> mp;
-        for(auto i:hand){
-            mp[i]++;
-        }
-        
-		// store {integer,count} in increasing order of integer in priority queue
-        for(auto i:mp){
-            pq.push(i);
-        }
-        
-		// stack used for storing popped elements of min heap
-        stack<pair<int,int>> st;
-        while(pq.size()){
-			// group size should be greater than min heap size
-            if(pq.size()<groupSize) return false;
-			int last=-1; // stores last stored integer popped from min heap 
-            for(int i=0; i<groupSize; i++){
-                auto top=pq.top();
-                pq.pop();
-				// check if last integer is set and current and last integers are not consecutive 
-                if(last>=0 && top.first-last!=1) return false;
-				last=top.first;  // store the current integer
-				// push current {integer,count} if its count is greater than 1 
-                if(top.second>1) st.push({top.first,top.second-1});
-            }
-			// push elements from stack back to queue
-            while(!st.empty()){
-                pq.push(st.top());
-                st.pop();
-            }
-        }
-        return true;
-    }
+	bool isNStraightHand(vector<int>& nums, int k) 
+	{
+		map<int,int> mp;
+		for(int i=0;i<nums.size();i++)
+		{
+			mp[nums[i]]++;
+		}
+
+		while(mp.size()>0)
+		{
+			auto first=mp.begin()->first;
+				for(int i=0;i<k;i++)
+				{
+					if(mp[first+i]>0)
+					{
+						mp[first+i]--;
+                        if(mp[first+i]==0)mp.erase(first+i);
+					}
+					else{
+                     return false;   
+                    }
+						
+				}
+		}
+
+
+		return true;
+
+	}
 };
