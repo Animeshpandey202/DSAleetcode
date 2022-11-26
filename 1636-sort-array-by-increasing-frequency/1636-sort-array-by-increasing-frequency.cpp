bf20201971923426220aa902f8e48016f4fd3866
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-11-27 04:33:16 +0530

    Time: 7 ms (94.10%), Space: 11.5 MB (41.86%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+     //why have you used static here?
+ //because it was giving error if we don't add that keyword. Basically, we don't need to create instance for accessing the static members, but if we dont make it static we have to make instance to access that function. 
+    static bool cmp(pair<int,int>&a, pair<int,int>&b) {
+        return  (a.second==b.second) ? a.first>b.first : a.second<b.second;
+    }
+    
+    vector<int> frequencySort(vector<int>& nums) {
+   
+        if(nums.size()==1) 
+            return nums;
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++) 
+        {
+            mp[nums[i]]++;
+        }
+        vector<pair<int,int>> val_freq;
+        for(auto m : mp) 
+        {
+            val_freq.push_back(m);
+        }
+        sort(val_freq.begin(),val_freq.end(),cmp);
+        vector<int> result;
+        for(auto v : val_freq) {
+            for(int i=0;i<v.second;i++) {
+                result.push_back(v.first);
+            }
+        }
+        return result;
+        
+    }
+};
+ 
+
+ 
