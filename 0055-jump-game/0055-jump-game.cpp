f815c2eaef71b679347809afd8de8002c10d2010
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-12-02 17:26:09 +0530

    Time: 168 ms (55.10%), Space: 48.5 MB (48.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         
-           int index=nums.size()-1;
+           int n=nums.size();
+            int idx=n-1;
 
-           for(int i=index-1;i>=0;i--)
-               if(i + nums[i] >= index)
-                   index = i;
-           
-
-         return index == 0;  
+        //we try to check from last...ie can we reach from last to start
+        
+           for(int i=n-2;i>=0;i--){
+                //if i +max jump at i >= index ..then we can reach till index idx
+               if(i+ nums[i]>=idx){
+                   idx=i;    //now we check can reach to new updated index idx or not from lest over arr in left side
+               }
+           }
+               
+        //if we could reach till index 0 return true else false
+         return idx == 0;  
     }
 };
