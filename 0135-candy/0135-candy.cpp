baa358046158df7082f9a093f125f257ccfdedea
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2022-12-05 17:03:09 +0530

    Time: 25 ms (87.01%), Space: 17.7 MB (62.68%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,7 +3,7 @@ public:
     int candy(vector<int>& arr) {
         int n=arr.size();
         vector<int>left(n,1);
-        vector<int>right(n,1);
+        
         
         for(int i=1;i<n;i++){
             if(arr[i]>arr[i-1]){
@@ -13,14 +13,38 @@ public:
         
         for(int i=n-2;i>=0;i--){
             if(arr[i]>arr[i+1]){
-                right[i]=right[i+1]+1;
+                left[i]=max(left[i],left[i+1]+1);
             }
+            
         }
         int count=0;
         for(int i=0;i<n;i++){
-            count+=max(left[i],right[i]);
+            count+=left[i];
             
         }
         return count;
     }
 };
+//using 2 arrays
+// int n=arr.size();
+//         vector<int>left(n,1);
+//         vector<int>right(n,1);
+        
+//         for(int i=1;i<n;i++){
+//             if(arr[i]>arr[i-1]){
+//                 left[i]=left[i-1]+1;
+//             }
+//         }
+        
+//         for(int i=n-2;i>=0;i--){
+//             if(arr[i]>arr[i+1]){
+//                 right[i]=right[i+1]+1;
+//             }
+            
+//         }
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             count+=max(left[i],right[i]);
+            
+//         }
+//         return count;
