f433c50e956156968e421ee0bb0978a0ba619cab
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-02-03 02:24:55 +0530

    Time: 0 ms (100.00%), Space: 13.1 MB (59.23%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,77 +1,49 @@
 class Solution {
 public:
-    
-    
-    bool isValid(int i, int j, int n, int m, vector<vector<int>>& grid){
-        if(i>=0 && i<n && j>=0 && j<m && grid[i][j] == 1){
-            return true;
-        }
-        return false;
-    }
-    
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
         
-        int fresh = 0, time = 0;
-        queue<pair<int, int>>q;
+        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j] == 2){
-                    // push into queue
-                    q.push({i, j});
-                }
-                else if(grid[i][j] == 1){
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        queue<pair<int,int>> q;
+        int fresh=0; //To keep track of all fresh oranges left
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                if(grid[i][j]==1)
                     fresh++;
-                }
             }
-        }
-        if(fresh == 0) return 0;
-        
-        // start BFS traversal
-        while(!q.empty()){
-            
-            int size_q = q.size();
-            int temp = 0;
-            while(size_q != 0){
-                
-                pair<int, int>p = q.front();
+        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
+        while(!q.empty())
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                pair<int,int> p=q.front();
                 q.pop();
-                
-                int x1 = p.first;
-                int y1 = p.second;
-                
-                int dx[4] = { 1, -1, 0, 0};
-                int dy[4] = { 0, 0 , 1 , -1};
-                
-                for(int i=0; i<4; i++){
-                    int nx = dx[i] + x1;
-                    int ny = dy[i] + y1;
-                    
-                    if(isValid(nx, ny, n, m, grid)){
-                        temp=1;
-                        grid[nx][ny] = 2;
-                        q.push({nx, ny});
-                        fresh--;
+                for(int i=0;i<4;i++)
+                {
+                    int r=p.first+dir[i];
+                    int c=p.second+dir[i+1];
+                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
+                    {
+                        grid[r][c]=2;
+                        q.push({r,c});
+                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
                     }
+                    
                 }
-                
-                size_q--;
-                
             }
-            if(temp != 0) time++;
+            ans++; //incremented after each minute passes
         }
-        
-        
-        
-        // we checked if any fresh oranges are still there
-      
-        if(fresh!=0)return -1;
-        return time;
-        
-        
-         
+        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
+        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
+        return ans;
         
     }
 };
