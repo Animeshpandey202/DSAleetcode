fbfbe32fc09937dd98b1202275dfa784de4f69d3
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-02-09 00:42:55 +0530

    Time: 726 ms (40.81%), Space: 177.9 MB (40.69%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,28 +1,58 @@
 class Solution {
 private:
-    void tarjan(int curr, int parent, int time, vector<vector<int>>& AdjLis, vector<int>& low, vector<vector<int>> &ans){
-        low[curr] = time++;
-        for (auto x: AdjLis[curr]){
-            if (x==parent)
-                continue;
-            if (low[x]==0) tarjan(x, curr, time, AdjLis, low, ans);
-            low[curr] = min(low[curr], low[x]);
-            if (low[x]==time)
-                ans.push_back({curr,x});
+    void dfs(int node,int parent,vector<int>&disc,vector<int>&low,int timer,vector<bool>&vis,vector<int>adj[],vector<vector<int>>&result){
+    vis[node]=true;
+    disc[node]=low[node]=timer++;
+    
+    for(auto nbr:adj[node]){
+        if(nbr==parent){
+            continue;
+        }
+        if(!vis[nbr]){
+            dfs(nbr,node,disc,low,timer,vis,adj,result);
+            //while returning check for a shorter path
+            low[node]=min(low[node],low[nbr]);
+            
+            //check bridge
+            if(low[nbr]>disc[node]){  //nbr can only come after parent
+                vector<int>ans;
+                ans.push_back(nbr);
+                ans.push_back(node);
+                result.push_back(ans);
+                
+            }
+             
+        }
+        else{
+            //nbr is already visited and not parent
+            //back edge
+            low[node]=min(low[node],disc[nbr]);
         }
     }
+}
 public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        // 1. Constructing graph using Adjacency list
-        vector<vector<int>> AdjLis(n);
-        for (auto x: connections){
-            AdjLis[x[0]].push_back(x[1]);
-            AdjLis[x[1]].push_back(x[0]);
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& edges) {
+      vector<int>adj[n];
+ 
+    
+    for(int i=0;i<edges.size();i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    vector<int>disc(n,-1);
+    vector<bool>vis(n,false);
+    vector<int>low(n,-1);
+    int parent=-1;
+    int timer=0;
+    vector<vector<int>>result;
+    //dfs
+    for(int i=0;i<n;i++){
+        if(!vis[i]){
+            dfs(i,parent,disc,low,timer,vis,adj,result);
         }
-        // 2. finding critical connections
-        vector<vector<int>> ans;
-        vector<int> low(n);
-        tarjan(0, -1, 1, AdjLis, low, ans);
-        return ans;
+    }
+    return result;
     }
 };
