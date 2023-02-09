776788d25a6188c688e14444be06ca8ea05af4e8
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-02-10 04:28:32 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (17.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,34 @@
 class Solution {
-    private:
-    //fn will return no of ways to reach position n
-    
-    int f(int n,vector<int>&dp){
-        
-        if(n==0|| n==1)return 1;
-        
-        if(dp[n]!=-1)return dp[n];
-        
-        int left=f(n-1,dp);
-        int right=f(n-2,dp);
-        
-        return dp[n]=left+right;
-    }
-public:
+  public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-       return f(n,dp);       
+       dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
+
+//top 2 bottom
+//   private:
+//     //fn will return no of ways to reach position n
+    
+//     int f(int n,vector<int>&dp){
+        
+//         if(n==0|| n==1)return 1;
+        
+//         if(dp[n]!=-1)return dp[n];
+        
+//         int left=f(n-1,dp);
+//         int right=f(n-2,dp);
+        
+//         return dp[n]=left+right;
+//     }
+// public:
+//     int climbStairs(int n) {
+//         vector<int>dp(n+1,-1);
+//        return f(n,dp);       
+//     }
