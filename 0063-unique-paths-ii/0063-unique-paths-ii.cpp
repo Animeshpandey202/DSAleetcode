127b007850d5faa327550b06c163dc272f6f4100
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-02-13 00:39:36 +0530

    Time: 0 ms (100.00%), Space: 8 MB (16.44%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    // fn tells no of ways to reach from a[0][0] to a[i][j]
+    
+        int f(int i,int j,vector<vector<int>>&maze,vector<vector<int>>& dp){
+           
+            if(i>=0 && j>=0 && maze[i][j]==1) return 0; 
+          if(i==0 && j == 0) return 1;
+          if(i<0 || j<0) return 0;
+            
+          if(dp[i][j]!=-1) return dp[i][j];
+
+          int up = f(i-1,j,maze,dp);
+          int left = f(i,j-1,maze,dp);
+
+          return dp[i][j] = up+left;
+
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>&maze) {
+        int n=maze.size();
+        int m=maze[0].size();
+        vector<vector<int> > dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,maze,dp);
+    
+    }
+};
+
+ 
