a56fad9c1b9cd9d5eccf0f0b23efee67310235f9
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-02-16 02:26:10 +0530

    Time: 7 ms (69.58%), Space: 9.1 MB (12.16%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,47 @@
 class Solution {
+    int minimumPathSumUtil(int i, int j, vector<vector<int> > &triangle,int n, 
+vector<vector<int> > &dp) {
+  
+  if(dp[i][j]!=-1)
+  return dp[i][j];
+  
+  if(i==n-1) return triangle[i][j];
+    
+  int down = triangle[i][j]+minimumPathSumUtil(i+1,j,triangle,n,dp);
+  int diagonal = triangle[i][j]+minimumPathSumUtil(i+1,j+1,triangle,n,dp);
+  
+  return dp[i][j] = min(down, diagonal);
+  
+}
+
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
+     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int> > dp(n,vector<int>(n,0));
-    
-    for(int j=0;j<n;j++){
-        dp[n-1][j] = triangle[n-1][j];
-    }
-    
-    for(int i=n-2; i>=0; i--){
-        for(int j=0; j<=i; j++){
-            
-            int down = triangle[i][j]+dp[i+1][j];
-            int diagonal = triangle[i][j]+dp[i+1][j+1];
-            
-            dp[i][j] = min(down, diagonal);
-        }
-    }
-    
-    return dp[0][0];   
-    }
+        vector<vector<int> > dp(n,vector<int>(n,-1));
+        return minimumPathSumUtil(0,0,triangle,n,dp);
+     }
+   
 };
+
+
+//tabulation
+//  int minimumTotal(vector<vector<int>>& triangle) {
+//         int n=triangle.size();
+//         vector<vector<int> > dp(n,vector<int>(n,0));
+    
+//     for(int j=0;j<n;j++){
+//         dp[n-1][j] = triangle[n-1][j];
+//     }
+    
+//     for(int i=n-2; i>=0; i--){
+//         for(int j=0; j<=i; j++){
+            
+//             int down = triangle[i][j]+dp[i+1][j];
+//             int diagonal = triangle[i][j]+dp[i+1][j+1];
+            
+//             dp[i][j] = min(down, diagonal);
+//         }
+//     }
+    
+//     return dp[0][0];   
+//     }
