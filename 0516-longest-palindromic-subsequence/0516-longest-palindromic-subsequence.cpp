8d2c98c8edb6511aae24eaea500e158704487d84
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-02-24 23:48:58 +0530

    Time: 221 ms (31.88%), Space: 72.9 MB (64.59%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,40 +1,33 @@
 class Solution {
-public:
-    int longestPalindSubseqRec(vector<vector<int> >& dp, string& s, int i, int j) {
-        if(i > j)
-            return 0;
-        // when one char
-        if(i == j)
-            return dp[i][j] = 1;
-        
-        if(dp[i][j] == -1) {
-            if(s[i] == s[j])
-                dp[i][j] = 2 + longestPalindSubseqRec(dp, s, i+1, j-1);
-            else
-                dp[i][j] = max(longestPalindSubseqRec(dp, s, i+1, j),
-                               longestPalindSubseqRec(dp, s, i, j-1));
-        }
-        return dp[i][j];
-    }
+    int lcs(string s1, string s2) {
     
-    // Recursive driver
-    int longestPalindSubseqRecDriver(string& s) {
-        if(s.empty())
-                return 0;
+    int n=s1.size();
+    int m=s2.size();
 
-        const int N = s.size();
-        // dp(i, j): length of longest palindromic substring in s[i:j]
-        vector<vector<int> > dp(N, vector<int>(N, -1)); 
-        
-        return longestPalindSubseqRec(dp, s, 0, N-1);
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
     }
     
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+}
+
+public:
     int longestPalindromeSubseq(string s) {
-        return longestPalindSubseqRecDriver(s);
-        //return longestPalindromeSubseqTabular(s);
+        string t = s;
+        reverse(s.begin(),s.end());
+        return lcs(s,t);
     }
 };
-   // Using memoization
-    // TC: O(N*N), SC: O(N*N)
-    
-// };
