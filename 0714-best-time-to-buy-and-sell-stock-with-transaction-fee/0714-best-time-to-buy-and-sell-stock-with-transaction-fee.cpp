b664d3183ec4fc08614e11d31a1586be1b3e34f4
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-03-18 20:44:27 +0530

    Time: 234 ms (48.03%), Space: 90.9 MB (43.58%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,27 +1,43 @@
 class Solution {
-    int fn(int ind,int buy,vector<int>&arr,int n,vector<vector<int>>&dp,int fee){
-        if(ind==n)return 0;
+//     int fn(int ind,int buy,vector<int>&arr,int n,vector<vector<int>>&dp,int fee){
+//         if(ind==n)return 0;
         
-        if(dp[ind][buy]!=-1)return dp[ind][buy];
-        int profit=0; 
+//         if(dp[ind][buy]!=-1)return dp[ind][buy];
+//         int profit=0; 
         
-        if(buy==0){
-            profit=max(0+fn(ind+1,0,arr,n,dp,fee), -arr[ind] +fn(ind+1,1,arr,n,dp,fee)); 
-        }
-        if(buy==1){
-            profit=max(0 +fn(ind+1,1,arr,n,dp,fee),+arr[ind] -fee +fn(ind+1,0,arr,n,dp,fee));
-        }
+//         if(buy==0){
+//             profit=max(0+fn(ind+1,0,arr,n,dp,fee), -arr[ind] +fn(ind+1,1,arr,n,dp,fee)); 
+//         }
+//         if(buy==1){
+//             profit=max(0 +fn(ind+1,1,arr,n,dp,fee),+arr[ind] -fee +fn(ind+1,0,arr,n,dp,fee));
+//         }
         
-        return dp[ind][buy]=profit;
+//         return dp[ind][buy]=profit;
         
         
-    }
+//     }
     
     
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return fn(0,0,prices,n,dp,fee);
+    int maxProfit(vector<int>& arr, int fee) {
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0; 
+        
+                if(buy==0){
+                    profit=max(0+dp[ind+1][0], -arr[ind] +dp[ind+1][1]);
+                }
+                if(buy==1){
+                    profit=max(0 +dp[ind+1][1],+arr[ind]-fee +dp[ind+1][0]) ;
+                }
+
+                 dp[ind][buy]=profit;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
