20ff534d33744cf5cbcf89030ff6f01bd3754ec2
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-04-15 22:54:07 +0530

    Added solution - LeetHub

---
+++ Pattern 17 - GFG/pattern-17.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int N) {
+         for(int i=0;i<N;i++){
+          
+          // for printing the spaces.
+          for(int j=0;j<N-i-1;j++){
+              cout<<" ";
+          }
+          
+          // for printing the characters.
+          char ch = 'A';
+          int breakpoint = (2*i+1)/2;
+          for(int j=1;j<=2*i+1;j++){
+              
+              cout<<ch;
+              if(j <= breakpoint) ch++;
+              else ch--;
+          }
+          
+          // for printing the spaces again after characters.
+        //   for(int j=0;j<N-i-1;j++){
+        //       cout<<" ";
+        //   }
+          
+          // As soon as the letters for each iteration are printed, we move to the
+          // next row and give a line break otherwise all letters
+          // would get printed in 1 line.
+          cout<<endl;
+          
+      }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
